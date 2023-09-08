b97d91ec2b4fe9de36116776a7024e2e07295edb
sumeyahajji <abdinasirsumeya@gmail.com>
2023-09-08 08:17:23 +0300

    completed exercise2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(" Hey, What is your favourite school subject?");
+            var favouriteSubject =Console.ReadLine();
+            switch(favouriteSubject)
+            {
+                case "math":
+                    Console.WriteLine("Wow!i see you love math");
+                    break;
+                case "biology":
+                    Console.WriteLine("Is biology your favourite subject?");
+                    break;
+                case "history":
+                    Console.WriteLine("I like history");
+                    break;
+                case "physics":
+                    Console.WriteLine("Physics is a bit challenging");
+                    break;
+                case "chemistry":
+                    Console.WriteLine("Chemistry is my favourite subject!");
+                    break;
+                default:
+                    Console.WriteLine("Hello, This is not your favourite subject, we will add this");
+                 break;   
+            }
         }
     }
 }
